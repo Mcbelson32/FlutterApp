9582e4d8a8f2a59b0b674f0d739ba7886f61ba6f
Mc_Belson <mcbelson32@gmail.com>
2023-11-20 07:59:05 +0000

    flutter

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,10 @@
 
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: MyHomePage(title: "home page")));
+void main() => runApp(MaterialApp(
+      home: MyHomePage(title: "home page"),
+      debugShowCheckedModeBanner: false,
+    ));
 
 class MyHomePage extends StatelessWidget {
   final String title;
@@ -15,6 +18,7 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         // The title text which will be shown on the action bar
         title: Text(title),
+        centerTitle: true,
         backgroundColor: Colors.grey[850],
         elevation: 0.0,
       ),
@@ -47,8 +51,52 @@ class MyHomePage extends StatelessWidget {
               ),
             ),
             SizedBox(
+              height: 30.0,
+            ),
+            Text(
+              "Mc's CURRENT LEVEL: ",
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            SizedBox(
               height: 10.0,
             ),
+            Padding(
+              padding: EdgeInsets.only(left: 10.0),
+              child: Text(
+                '8',
+                style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Row(
+              children: [
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text(
+                  "abc123@gmail.com",
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    letterSpacing: 1.0,
+                  ),
+                ),
+              ],
+            )
           ],
         ),
       ),
